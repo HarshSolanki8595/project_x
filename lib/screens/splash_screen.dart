17c859f403bea06bb2ca8c80cec61ee5946a6abe
Harsh Solanki <h.solanki724@gmail.com>
2026-07-15 20:29:01 +0530

    Initial Flutter project setup

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Icon(
+              Icons.home_repair_service,
+              color: Colors.white,
+              size: 100,
+            ),
+            SizedBox(height: 20),
+            Text(
+              "Project X",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              "Connecting Homes. Empowering Professionals.",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white70,
+                fontSize: 16,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
